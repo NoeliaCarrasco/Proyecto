8caa816c661c5d11d02aec01ae49f53456d6f2d7
NoeliaCarrasco <noelia-besos-doce@hotmail.com>
2016-04-20 20:32:55 +0200

    Delete quitarCarrito_penultimointento.php

--- quitarCarrito_penultimointento.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if(!isset($_SESSION['rol'])){header('location: login.php');}
-	if(!isset($_SESSION['carrito'])){ $_SESSION['carrito'] = [];}
-	if(isset($_SESSION['carrito'][$_REQUEST['i']])){
-		unset($_SESSION['carrito'][$_REQUEST['i']]);
-	}
-	header('location: shopping-cart.php');
-?>
