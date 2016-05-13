c9d00408c0774c774fd49fdca797efd7a5ff51a4
NoeliaCarrasco <noelia-besos-doce@hotmail.com>
2016-05-13 13:33:53 +0200

    ultimo
    borrar pedidos solo el administrador

---
+++ borrarPedidos.php
@@ -0,0 +1,13 @@
+<?php
+include_once("./db_configuration.php");
+	session_start();
+	if(!isset($_SESSION['rol'])){header('location: login.php');}else{if(intval($_SESSION['rol']) != 2){header('location: index.php');}}
+	if(isset($_REQUEST['i'])){
+		$connection = new mysqli($db_host, $db_user, $db_password, "deportes");
+		$deletes="DELETE FROM detallespedido WHERE IDPEDIDO = '".$_REQUEST['i']."'";
+		$connection->query($deletes);
+		$delete="DELETE FROM pedidos WHERE IDPEDIDO = '".$_REQUEST['i']."'";
+		$connection->query($delete);
+	}
+	header('location: '.$_SERVER['HTTP_REFERER']);
+?>
