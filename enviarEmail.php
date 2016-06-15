e9fb3a4636bd36183a81c3639a61164f43d902cb
NoeliaCarrasco <noelia-besos-doce@hotmail.com>
2016-06-15 23:43:03 +0200

    Update enviarEmail.php

--- enviarEmail.php
+++ enviarEmail.php
@@ -2,11 +2,11 @@
 	session_start();
 	$para      = 'noelia-besos-doce@hotmail.com';
 	$titulo    = 'Contacto desde la web';
-	$mensaje   = $_REQUEST['mensaje']."\n\n\nDe: ".$_REQUEST['nombre']."\n\nTelefono: ".$_REQUEST['telefono'];//En el campor mensaje coge lo que escribes al igual q en nombre y telefono
+	$mensaje   = $_REQUEST['mensaje']."\n\n\nDe: ".$_REQUEST['nombre']."\n\nTelefono: ".$_REQUEST['telefono'];
 	$cabeceras = 'From: '.$_REQUEST['email'] . "\r\n" .
 		'Reply-To:'.$_REQUEST['email']."\r\n" .
-		'X-Mailer: PHP/' . phpversion();//el campo email aparece quien te lo envia y a quien respondes en este caso coge el mio
+		'X-Mailer: PHP/' . phpversion();
 
-	mail($para, $titulo, $mensaje, $cabeceras);//coge lo que hay dentro de cada variable
+	mail($para, $titulo, $mensaje, $cabeceras);
 	header('location: contact.php');
 ?>
