95584c1ec47e3e0cb047543be300a7b944e79a9b
NoeliaCarrasco <noelia-besos-doce@hotmail.com>
2016-06-16 00:07:38 +0200

    Update quitarCarrito.php

--- quitarCarrito.php
+++ quitarCarrito.php
@@ -3,7 +3,7 @@
 	if(!isset($_SESSION['rol'])){header('location: login.php');}
 	if(!isset($_SESSION['carrito'])){ $_SESSION['carrito'] = [];}
 	if(isset($_SESSION['carrito'][$_REQUEST['i']])){
-		unset($_SESSION['carrito'][$_REQUEST['i']]);//destruye el produto con el código que guarda la clave 'i' del array REQUEST almacenada en SESSION
+		unset($_SESSION['carrito'][$_REQUEST['i']]);
 	}
 	header('location: shopping-cart.php');
 ?>
